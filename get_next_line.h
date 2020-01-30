df28e43c98bf0df392741e90fb622a4827b60964
Gynir Starvin <gstarvin@at-j3.21-school.ru>
2020-01-30 14:30:59 +0300

    a

--- get_next_line.h
+++ get_next_line.h
@@ -1,22 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gstarvin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/06 10:40:13 by gstarvin          #+#    #+#             */
-/*   Updated: 2019/09/06 10:42:33 by gstarvin         ###   ########.fr       */
+/*   Created: 2019/09/23 15:14:01 by gstarvin          #+#    #+#             */
+/*   Updated: 2019/09/23 15:14:07 by gstarvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include "libft/libft.h"
+# include <fcntl.h>
 
-int		ft_isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') ||
-		(c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
+int		get_next_line(const int fd, char **line);
+
+#endif
